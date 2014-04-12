8bd4bfe2c6b2ca43310a1a0a7b53b53055f62c8d
John Beieler <john.b30@gmail.com>
2014-04-12 03:33:55 -0400

    Major rewrite of formatter.py

--- formatter.py
+++ formatter.py
@@ -1,188 +1,144 @@
 import re
 import sys
+import timex
 import textwrap
+import datetime
 import phox_utilities
 from dateutil import parser
 from collections import Counter
 
 
-MAX_LEDE = 4   # number of sentences to output
-
-sources = {
-    'csmonitor.com': 'CSM',
-    'bbc.co.uk': 'BBC',
-    'reuters.com': 'REU',
-    'xinhuanet.com': 'XIN',
-    'www.upi.com': 'UPI',
-    'nytimes.com': 'NYT',
-    'todayszaman.com': 'TZA',
-    'hosted2.ap.org': 'APP',
-    'theguardian.com': 'GUA',
-    'todayszaman.com': 'TZA',
-    'insightcrime.org': 'ISC',
-    'france24.com': 'FRA',
-    'yahoo.com': 'YAH',
-    'allafrica.com': 'ALA',
-    'voanews.com': 'VOA',
-    'aljazeera.com': 'AJZ',
-    'AlAkhbarEnglish': 'AKB',
-    'usatoday.com': 'USA',
-    'latimes.com': 'LAT',
-    'foxnews.com': 'FOX',
-    'IRINnews.org': 'IRI',
-    'rfi.fr': 'RFI',
-    'cnn.com': 'CNN',
-    'abcnews': 'ABC',
-    'wsj.com': 'WSJ',
-    'nydailynews.com': 'NYD',
-    'washingtonpost.com': 'WAS',
-    'chicagotribune.com': 'CHT',
-    'miamiherald.com': 'MIA',
-    'thestar.com.my': 'STA',
-    'al-monitor.com': 'MON',
-    'un.org': 'UNN',
-    'pakistantelegraph.com': 'PAK',
-    'africaleader.com': 'AFR',
-    'theuknews.com': 'UKN',
-    'indiagazette.com': 'ING',
-    'mexicostar.com': 'MES',
-    'japanherald.com': 'JAH',
-    'chinanationalnews.com': 'CHN',
-    'jpost.com': 'JPO',
-    'middleeaststar.com': 'MEA',
-    'beijingbulletin.com': 'BEB',
-    'arabherald.com': 'ARH',
-    'russiaherald.com': 'RUH',
-    'iraqsun.com': 'IRA',
-    'malaysiasun.com': 'MAL',
-    'thehindu.com': 'HIN',
-    'shanghaisun.com': 'SHA',
-    'dawn.com': 'DAW',
-    'singaporestar.com': 'SNG',
-    'southeastasiapost.com': 'SEA',
-    'afghanistansun.com': 'AFG',
-    'australianherald.com': 'AUS',
-    'thenational.ae': 'NAT',
-    'europesun.com': 'EUS',
-    'greekherald.com': 'GKH',
-    'irishsun.com': 'IRS',
-    'iranherald.com': 'IAH',
-    'parisguardian.com': 'PAG',
-    'brazilsun.com': 'BZS',
-    'asiabulletin.com': 'ASB',
-    'nigeriasun.com': 'NIG',
-    'haitisun.com': 'HAI',
-    'philippinetimes.com': 'PHI',
-    'venezuelastar.com': 'VEN',
-    'caribbeanherald.com': 'CAR',
-    'zimbabwestar.com': 'ZIM',
-    'torontotelegraph.com': 'TOR',
-    'bangladeshsun.com': 'BAN',
-    'sydneysun.com': 'SYD',
-    'menafn.com': 'MEN',
-    'thelocal.se': 'TLO',
-    'nepalnational.com': 'NEP',
-    'kenyastar.com': 'KEN',
-    'sierraleonetimes.com': 'SIE',
-    'hurriyetdailynews.com': 'HDN',
-    'pajhwok.com': 'PAJ',
-    'thelocal.it': 'LIT',
-    'thelocal.fr': 'LFR',
-    'thelocal.es': 'LES',
-    'thelocal.ch': 'LCH',
-    'ipsnews.net': 'IPS',
-    'jamaicantimes.com': 'JAM',
-    'argentinastar.com': 'ARG',
-    'srilankasource.com': 'SRI',
-    'centralasiatimes.com': 'CEN',
-    'thelocal.no': 'LNO',
-    'theeastafrican.co.ke': 'ESF',
-    'taiwansun.com': 'TAI',
-    'cambodiantimes.com': 'CAM',
-    'payvand.com': 'PAY',
-    'trinidadtimes.com': 'TRD',
-    'yementimes.com': 'YEM',
-    'hongkongherald.com': 'HNK'}
-
-
-def get_date(field):
+def format_records(results, max_lede, process_date):
     """
-    Function to extract date from story header in scraper_results.
+
+    Parameters
+    -----
+
+    results: pymongo.cursor.Cursor. Iterable.
+            Iterable containing the results from the scraper.
+
+    max_lede: Integer.
+                How many sentences to include from the news story.
+
+    process_date: datetime object.
+                    Datetime object indicating which date the pipeline is
+                    processing. Standard is date_running - 1 day.
+
+    Returns
+    ------
+
+    final_output: String.
+                    TABARI-formatted records with one sentence per entry.
+
+    sourcecount: collections.Counter object.
+                    Dictionary of sources with the source (as defined within
+                    scraper whitelist_urls.csv file) as the key and the count
+                    of how many stories came from that source as the value.
+
+    """
+    sourcecount = Counter()
+
+    max_lede = 4
+    final_output = ''
+    for story in results:
+        content = story['content']
+        final_content = _format_content(content)
+
+        MAX_URLLENGTH = 192   # temporary to accommodate TABARI input limits
+        url = story['url'][:MAX_URLLENGTH]
+
+        date = _get_date(story, process_date)
+
+        source = story['source']
+        sourcecount[source] += 1
+
+        #Filter sentences that start with a "
+        final_content = [sent for sent in final_content if sent[0] != '"']
+        for sent in final_content[:max_lede]:
+            sent = sent.encode('utf-8')
+            line = '\n'.join(textwrap.wrap(sent, 80))
+            line = line.decode('utf-8')
+            line = line.encode('utf-8')
+            try:
+                url = url.encode('utf-8')
+            except UnicodeDecodeError:
+                url = url.decode('utf-8')
+            try:
+                one_record = '{} {}\n{}\n\n'.format(date, url, line)
+            except UnicodeEncodeError:
+                print 'Error on: {}, {}, {}'.format(date,
+                                                    url,
+                                                    line)
+
+            final_output += one_record
+
+    return final_output, sourcecount
+
+
+def _format_content(raw_content):
+    """
+    Function to process a given news story for further formatting.
 
     Parameters
     ------
-    field: String
-            Story header.
+
+    raw_content: String.
+                    Content of a news story as pulled from the web scraping
+                    database.
+
+    Returns
+    --------
+
+    sent_list: List.
+                List of sentences.
+
+    """
+    content = _get_story(raw_content)
+    sent_list = _sentence_segmenter(content)
+    return sent_list
+
+
+def _get_story(story_all):
+    """
+    Function to extract story text without date and source line.
+
+    Parameters
+    ------
+
+    story_all: String.
+                Content of a news story as pulled from the web scraping
+                database.
 
     Returns
     -------
 
-    date : String
-            Date string in the form YYMMDD
+    story: String.
+            Content of story with header/frontmatter removed.
+
     """
-    if 'csmonitor.com' in field[-1]:
-        csmdate = field[-1].split('/20')[1].split('/')
-        date = csmdate[0] + csmdate[1]
-    elif field[1]:
+
+    if '(Reuters)' in story_all:
+        story = story_all[story_all.find('(Reuters)') + 12:]
+    elif '(IANS)' in story_all:
+        story = story_all[story_all.find('(IANS)') + 7:]
+    elif '(ANI)' in story_all:
+        story = story_all[story_all.find('(ANI)') + 7:]
+    elif '(Xinhua) -- ' in story_all:
+        story = story_all[story_all.find('(Xinhua) -- ') + 12:]
+    elif '(UPI) -- ' in story_all:
+        story = story_all[story_all.find('(UPI) -- ') + 9:]
+    if bool(re.search("\xe2\x80\x93", story_all[0:32])):
         try:
-            date_obj = parser.parse(field[1])
-            date = str(date_obj)[2:4] + str(date_obj)[5:7] + str(date_obj)[8:10]
-        except:
-            date = '000000'
+            story = story_all.split("\xe2\x80\x93 ", 1)[1]
+        except IndexError:
+            story = story_all
     else:
-        date = '000000'
+        story = story_all
 
-    return date
-
-
-def write_record(source, sourcecount, thisdate, thisURL, story, fout):
-    """
-    Function to write TABARI-formatted story record.
-
-    Parameters
-    ------
-    source: String
-            Story news source.
-
-    sourcecount: Dictionary
-            Dictionary of counts of stories by source
-
-    thisdate: String
-            Date string in the form YYMMDD
-
-    thisURL: String
-            String of URL to story at source
-
-    story: String
-            News article story
-
-    fout: file
-            File for TABARI-formatted output
-    """
-    sourcecount[source] += 1
-
-    sentlist = sentence_segmenter(story)
-
-    nsent = 1
-    for sent in sentlist:
-        if sent[0] != '"':  # skip sentences beginning with quotes
-            #print thisdate, source, thisURL
-           # print >> fout, thisdate + ' ' + source + '-' + \
-           #     str(sourcecount[source]).zfill(4) + '-' + \
-           #     str(nsent) + ' ' + thisURL  # + '\n'
-            print >> fout, thisdate + ' ' + thisURL
-            lines = textwrap.wrap(sent, 80)
-            for txt in lines:
-                print >> fout, txt
-
-            print >> fout, '\n'
-
-        nsent += 1
-        if nsent > MAX_LEDE:
-            break
+    return story
 
 
-def sentence_segmenter(paragr):
+def _sentence_segmenter(paragr):
     """
     Function to break a string 'paragraph' into a list of sentences based on
     the following rules:
@@ -199,7 +155,15 @@ def sentence_segmenter(paragr):
 
     Parameters
     ------
-    paragr: String
+
+    paragr: String.
+            Content that will be split into constituent sentences.
+
+    Returns
+    --------
+
+    sentlist: List.
+                List of sentences.
 
     """
     # this is relatively high because we are only looking for sentences that
@@ -265,132 +229,153 @@ def sentence_segmenter(paragr):
     return sentlist
 
 
-def get_source(field):
+#The only one where we don't have a firm, consistent source in the
+#story['source'] is the google news stuff.
+#Deprecated for now since I don't see a clear purpose for it
+#def _get_source(field):
+#    """
+#    Function to extract source from URL in story header.
+#
+#    Parameters
+#    ------
+#    field: String
+#            Story header.
+#
+#    Returns
+#    -------
+#
+#    source : String
+#            3 char source code from sources dictionary
+#    """
+#    source_url = field[-1].strip()
+#    sourced = source_url.split('http://')[-1]
+#
+#    try:
+#        key = [i for i, s in enumerate(sources.keys()) if s in sourced]
+#        if key:
+#            source = sources[sources.keys()[key[0]]]
+#        else:
+#            source = '999'
+#    except AttributeError:
+#        source = '999'
+#
+#    return source
+
+
+def _get_date(result_entry, process_date):
     """
-    Function to extract source from URL in story header.
+    Function to extract date from a story. First checks for a date from the RSS
+    feed itself. Then tries to pull a date from the first two sentences of a
+    story. Finally turns to the date that the story was added to the database.
+    For the dates pulled from the story, the function checks whether the
+    difference is greater than one day from the date that the pipeline is
+    parsing.
 
     Parameters
     ------
-    field: String
-            Story header.
+
+    result_entry: Dictionary.
+                    Record of a single result from the web scraper.
+
+    process_date: datetime object.
+                    Datetime object indicating which date the pipeline is
+                    processing. Standard is date_running - 1 day.
+
 
     Returns
     -------
 
-    source : String
-            3 char source code from sources dictionary
+    date : String.
+            Date string in the form YYMMDD.
+
     """
-    source_url = field[-1].strip()
-    sourced = source_url.split('http://')[-1]
-
-    try:
-        key = [i for i, s in enumerate(sources.keys()) if s in sourced]
-        if key:
-            source = sources[sources.keys()[key[0]]]
-        else:
-            source = '999'
-    except AttributeError:
-        source = '999'
-
-    return source
-
-
-def get_story(story_all):
-    """
-    Function to extract story text without date and source line.
-
-    Parameters
-    ------
-    story_all: String
-            All story text.
-
-    Returns
-
-    -------
-
-    story : String
-            3 char source code from sources dictionary
-    """
-
-    if '(Reuters)' in story_all:
-        story = story_all[story_all.find('(Reuters)') + 12:]
-    elif '(IANS)' in story_all:
-        story = story_all[story_all.find('(IANS)') + 7:]
-    elif '(ANI)' in story_all:
-        story = story_all[story_all.find('(ANI)') + 7:]
-    elif '(Xinhua) -- ' in story_all:
-        story = story_all[story_all.find('(Xinhua) -- ') + 12:]
-    elif '(UPI) -- ' in story_all:
-        story = story_all[story_all.find('(UPI) -- ') + 9:]
-    if bool(re.search("\xe2\x80\x93", story_all[0:32])):
+    date_obj = ''
+    if result_entry['date']:
         try:
-            story = story_all.split("\xe2\x80\x93 ", 1)[1]
-        except IndexError:
-            story = story_all
+            date_obj = parser.parse(result_entry['date'])
+        except TypeError:
+            date_obj = ''
     else:
-        story = story_all
+        date_obj = ''
 
-    return story
+    if not date_obj:
+        tagged = timex.tag(result_entry['content'][:2])
+        dates = re.findall(r'<TIMEX2>(.*?)</TIMEX2>', tagged)
+        if dates:
+            try:
+                date_obj = parser.parse(dates[0])
+                diff_check = _check_date(date_obj, process_date)
+                if diff_check:
+                    date_obj = ''
+            except TypeError:
+                date_obj = ''
+        else:
+            date_obj = ''
+
+    if not date_obj:
+        date_obj = result_entry['date_added']
+
+    date = '{}{:02d}{:02d}'.format(str(date_obj.year)[2:], date_obj.month,
+                                   date_obj.day)
+
+    return date
 
 
-def main(thisday, server_list, file_details):
+def _check_date(date_object, process_date):
     """
-    Main function to parse scraper_results to TABARI-formatted output.
-    """
-    scraperfilename = file_details.scraper_stem + thisday + '.txt'
-    print "Mongo: Scraper file name:", scraperfilename
+    Function to check the gap between the parsed date and the actual date that
+    the pipeline is processing.
 
+    Parameters
+    --------
+
+    date_object: datetime object.
+                    Date that the _get_date function suggests as a candidate
+                    date.
+
+    process_date: datetime object.
+                    Datetime object indicating which date the pipeline is
+                    processing. Standard is date_running - 1 day.
+
+    Returns
+    ------
+
+    too_big: Boolean.
+                Whether the gap is one day or larger.
+
+    """
+    diff = date_object - process_date
+    too_big = diff > datetime.timedelta(days=0)
+
+    return too_big
+
+
+def main(results, server_list, file_details, process_date, thisday):
+    """
+    Main function to parse results from the web scraper to TABARI-formatted
+    output.
+    """
     recordfilename = file_details.recordfile_stem + thisday + '.txt'
     print "Mongo: Record file name:", recordfilename
 
     #Suppress this for now.
-#    newsourcefile = file_details.newsourcestem + thisday + '.txt'
+#    newsourcefile = newsourcestem + thisday + '.txt'
 #    print "Mongo: New Sources file name:", newsourcefile
 
-    try:
-        fin = open(scraperfilename, 'r')
-    except IOError:
-        print scraperfilename
-        phox_utilities.do_RuntimeError(
-            'Could not find the scraper file for',
-            thisday)
+    MAX_LEDE = 4
+    results_to_write, source_counts = format_records(results, MAX_LEDE,
+                                                     process_date)
 
-    finlist = fin.readlines()
-    fout = open(recordfilename, 'w')
-    #newout = open(newsourcefile, 'w')
-    sourcecount = Counter()
+    source_counts_string = ''
+    for source, count in source_counts.iteritems():
+        source_counts_string += '{}\t{}'.format(source, count)
 
-    #storyno = 1
-    #csno = 1
+#    with open(newsourcefile, 'w') as sauce:
+#        sauce.write(source_counts_string)
 
-    MAX_URLLENGTH = 192
+    with open(recordfilename, 'w') as f:
+        f.write(results_to_write)
 
-    for line in range(0, len(finlist)):
-        if '\thttp' in finlist[line]:
-            field = finlist[line].split('\t')
-            thisURL = field[2][:-1]
-            # temporary to accommodate TABARI input limits
-            thisURL = thisURL[:MAX_URLLENGTH]
-
-            thisstory = get_story(finlist[line + 1])
-            thisdate = get_date(field)
-            thissource = get_source(field)
-
-            if thissource == '999':
-                # Adds sources not included in sources dictionary to
-                # 'newsource_results_20..' file output
-                print >> newout, thisURL
-
-            write_record(
-                thissource,
-                sourcecount,
-                thisdate,
-                thisURL,
-                thisstory,
-                fout)
-
-    fin.close()
-    fout.close()
     print "Finished"
 
 if __name__ == '__main__':
