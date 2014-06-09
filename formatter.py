ef7cd4ec8a05ac86e330daf28bf755c09d9670aa
John Beieler <john.b30@gmail.com>
2014-06-09 14:56:04 -0400

    Adding PETRARCH into the processing pipeline.

--- formatter.py
+++ formatter.py
@@ -1,83 +1,16 @@
 import re
-import sys
 import timex
-import textwrap
 import datetime
 import utilities
 from dateutil import parser
 from collections import Counter
 
 
-def format_records(results, max_lede, process_date):
+def format_content(raw_content):
     """
-
-    Parameters
-    ----------
-
-    results: pymongo.cursor.Cursor. Iterable.
-            Iterable containing the results from the scraper.
-
-    max_lede: Integer.
-                How many sentences to include from the news story.
-
-    process_date: datetime object.
-                    Datetime object indicating which date the pipeline is
-                    processing. Standard is date_running - 1 day.
-
-    Returns
-    -------
-
-    final_output: String.
-                    TABARI-formatted records with one sentence per entry.
-
-    sourcecount: collections.Counter object.
-                    Dictionary of sources with the source (as defined within
-                    scraper whitelist_urls.csv file) as the key and the count
-                    of how many stories came from that source as the value.
-
-    """
-    sourcecount = Counter()
-
-    max_lede = 4
-    final_output = ''
-    for story in results:
-        content = story['content'].encode('utf-8')
-        final_content = _format_content(content)
-
-        MAX_URLLENGTH = 192   # temporary to accommodate TABARI input limits
-        url = story['url'][:MAX_URLLENGTH]
-
-        date = _get_date(story, process_date)
-
-        source = story['source']
-        sourcecount[source] += 1
-
-        story_id = str(story['_id'])
-
-        #Filter sentences that start with a "
-        final_content = [sent for sent in final_content if sent[0] != '"']
-        for num, sent in enumerate(final_content[:max_lede]):
-            line = '\n'.join(textwrap.wrap(sent, 80))
-            line = line.decode('utf-8')
-            line = line.encode('utf-8')
-            sent_id = '{}_{}'.format(story_id, num)
-            try:
-                url = url.encode('utf-8')
-            except UnicodeDecodeError:
-                url = url.decode('utf-8')
-            try:
-                one_record = '{} {}\n{}\n\n'.format(date, sent_id, line)
-            except UnicodeEncodeError:
-                print 'Error writing one record.'
-
-            final_output += one_record
-
-    return final_output, sourcecount
-
-
-def _format_content(raw_content):
-    """
-    Function to process a given news story for further formatting.
+    Function to process a given news story for further formatting. Calls
+    a function that extract the story text minus the date and source line. Also
+    splits the sentences using the ``sentence_segmenter()`` function.
 
     Parameters
     ----------
@@ -94,8 +27,8 @@ def _format_content(raw_content):
 
     """
     content = _get_story(raw_content)
-    sent_list = _sentence_segmenter(content)
-    return sent_list
+    split = sentence_segmenter(content)
+    return split
 
 
 def _get_story(story_all):
@@ -138,7 +71,95 @@ def _get_story(story_all):
     return story
 
 
-def _sentence_segmenter(paragr):
+def get_date(result_entry, process_date):
+    """
+    Function to extract date from a story. First checks for a date from the RSS
+    feed itself. Then tries to pull a date from the first two sentences of a
+    story. Finally turns to the date that the story was added to the database.
+    For the dates pulled from the story, the function checks whether the
+    difference is greater than one day from the date that the pipeline is
+    parsing.
+
+    Parameters
+    ----------
+
+    result_entry: Dictionary.
+                    Record of a single result from the web scraper.
+
+    process_date: datetime object.
+                    Datetime object indicating which date the pipeline is
+                    processing. Standard is date_running - 1 day.
+
+
+    Returns
+    -------
+
+    date : String.
+            Date string in the form YYMMDD.
+
+    """
+    date_obj = ''
+    if result_entry['date']:
+        try:
+            date_obj = parser.parse(result_entry['date'])
+        except TypeError:
+            date_obj = ''
+    else:
+        date_obj = ''
+
+    if not date_obj:
+        tagged = timex.tag(result_entry['content'][:2])
+        dates = re.findall(r'<TIMEX2>(.*?)</TIMEX2>', tagged)
+        if dates:
+            try:
+                date_obj = parser.parse(dates[0])
+                diff_check = _check_date(date_obj, process_date)
+                if diff_check:
+                    date_obj = ''
+            except TypeError:
+                date_obj = ''
+        else:
+            date_obj = ''
+
+    if not date_obj:
+        date_obj = result_entry['date_added']
+
+    date = '{}{:02d}{:02d}'.format(str(date_obj.year)[2:], date_obj.month,
+                                   date_obj.day)
+
+    return date
+
+
+def _check_date(date_object, process_date):
+    """
+    Function to check the gap between the parsed date and the actual date that
+    the pipeline is processing.
+
+    Parameters
+    ----------
+
+    date_object: datetime object.
+                    Date that the _get_date function suggests as a candidate
+                    date.
+
+    process_date: datetime object.
+                    Datetime object indicating which date the pipeline is
+                    processing. Standard is date_running - 1 day.
+
+    Returns
+    -------
+
+    too_big: Boolean.
+                Whether the gap is one day or larger.
+
+    """
+    diff = date_object - process_date
+    too_big = diff > datetime.timedelta(days=0)
+
+    return too_big
+
+
+def sentence_segmenter(paragr):
     """
     Function to break a string 'paragraph' into a list of sentences based on
     the following rules:
@@ -229,165 +250,32 @@ def _sentence_segmenter(paragr):
     return sentlist
 
 
-#The only one where we don't have a firm, consistent source in the
-#story['source'] is the google news stuff.
-#Deprecated for now since I don't see a clear purpose for it
-#def _get_source(field):
-#    """
-#    Function to extract source from URL in story header.
-#
-#    Parameters
-#    ------
-#    field: String
-#            Story header.
-#
-#    Returns
-#    -------
-#
-#    source : String
-#            3 char source code from sources dictionary
-#    """
-#    source_url = field[-1].strip()
-#    sourced = source_url.split('http://')[-1]
-#
-#    try:
-#        key = [i for i, s in enumerate(sources.keys()) if s in sourced]
-#        if key:
-#            source = sources[sources.keys()[key[0]]]
-#        else:
-#            source = '999'
-#    except AttributeError:
-#        source = '999'
-#
-#    return source
-
-
-def _get_date(result_entry, process_date):
-    """
-    Function to extract date from a story. First checks for a date from the RSS
-    feed itself. Then tries to pull a date from the first two sentences of a
-    story. Finally turns to the date that the story was added to the database.
-    For the dates pulled from the story, the function checks whether the
-    difference is greater than one day from the date that the pipeline is
-    parsing.
-
-    Parameters
-    ----------
-
-    result_entry: Dictionary.
-                    Record of a single result from the web scraper.
-
-    process_date: datetime object.
-                    Datetime object indicating which date the pipeline is
-                    processing. Standard is date_running - 1 day.
-
-
-    Returns
-    -------
-
-    date : String.
-            Date string in the form YYMMDD.
-
-    """
-    date_obj = ''
-    if result_entry['date']:
-        try:
-            date_obj = parser.parse(result_entry['date'])
-        except TypeError:
-            date_obj = ''
-    else:
-        date_obj = ''
-
-    if not date_obj:
-        tagged = timex.tag(result_entry['content'][:2])
-        dates = re.findall(r'<TIMEX2>(.*?)</TIMEX2>', tagged)
-        if dates:
-            try:
-                date_obj = parser.parse(dates[0])
-                diff_check = _check_date(date_obj, process_date)
-                if diff_check:
-                    date_obj = ''
-            except TypeError:
-                date_obj = ''
-        else:
-            date_obj = ''
-
-    if not date_obj:
-        date_obj = result_entry['date_added']
-
-    date = '{}{:02d}{:02d}'.format(str(date_obj.year)[2:], date_obj.month,
-                                   date_obj.day)
-
-    return date
-
-
-def _check_date(date_object, process_date):
-    """
-    Function to check the gap between the parsed date and the actual date that
-    the pipeline is processing.
-
-    Parameters
-    ----------
-
-    date_object: datetime object.
-                    Date that the _get_date function suggests as a candidate
-                    date.
-
-    process_date: datetime object.
-                    Datetime object indicating which date the pipeline is
-                    processing. Standard is date_running - 1 day.
-
-    Returns
-    -------
-
-    too_big: Boolean.
-                Whether the gap is one day or larger.
-
-    """
-    diff = date_object - process_date
-    too_big = diff > datetime.timedelta(days=0)
-
-    return too_big
-
-
 def main(results, server_list, file_details, process_date, thisday):
     """
     Main function to parse results from the web scraper to TABARI-formatted
     output.
     """
-    recordfilename = file_details.recordfile_stem + thisday + '.txt'
-    print "Mongo: Record file name:", recordfilename
 
-    #Suppress this for now.
-#    newsourcefile = newsourcestem + thisday + '.txt'
-#    print "Mongo: New Sources file name:", newsourcefile
+    sourcecount = Counter()
 
-    MAX_LEDE = 4
-    results_to_write, source_counts = format_records(results, MAX_LEDE,
-                                                     process_date)
+    new_results = []
+    for i, story in enumerate(list(results)):
+        content = story['content'].encode('utf-8')
+        formatted_content = format_content(content)
+        story['content'] = ' '.join([sent for sent in formatted_content if
+                                     sent[0] != '"'])
 
-    source_counts_string = ''
-    for source, count in source_counts.iteritems():
+        story['date'] = get_date(story, process_date)
+
+        source = story['source']
+        sourcecount[source] += 1
+
+        new_results.append(story)
+
+    for source, count in sourcecount.iteritems():
         source_counts_string += '{}\t{}'.format(source, count)
 
 #    with open(newsourcefile, 'w') as sauce:
 #        sauce.write(source_counts_string)
 
-    with open(recordfilename, 'w') as f:
-        f.write(results_to_write)
-
-    print "Finished"
-
-if __name__ == '__main__':
-    if len(sys.argv) > 2:  # initializations for stand-alone tests
-        utilities.init_logger('test_pipeline.log')
-        logger = utilities.logger  # get a local copy for the pipeline
-        # initialize the various utilities globals
-        utilities.parse_config('test_config.ini')
-
-    if len(sys.argv) > 1:
-        thisday = sys.argv[1]
-    else:
-        utilities.do_RuntimeError('No date suffix in Mongo.formatter.py')
-
-    main(thisday)
+    return new_results
--- pipeline.py
+++ pipeline.py
@@ -4,56 +4,70 @@ import datetime
 import uploader
 import utilities
 import formatter
-import subprocess
 import oneaday_formatter
 import scraper_connection
 import petrarch
 
 
-utilities.init_logger('PHOX_pipeline.log')
-# get a local copy for the pipeline
-logger = logging.getLogger('pipeline_log')
+def main(write_petr=False, oneaday_filter=True):
+    utilities.init_logger('PHOX_pipeline.log')
+    # get a local copy for the pipeline
+    logger = logging.getLogger('pipeline_log')
 
-# initialize the various utilities globals
-server_details, file_details = utilities.parse_config('PHOX_config.ini')
+    # initialize the various utilities globals
+    server_details, file_details = utilities.parse_config('PHOX_config.ini')
 
-print '\nPHOX.pipeline run:', datetime.datetime.utcnow()
+    print '\nPHOX.pipeline run:', datetime.datetime.utcnow()
 
-if len(sys.argv) > 1:
-    date_string = sys.argv[1]
-    process_date = datetime.datetime.utcnow() - datetime.timedelta(days=1)
-    logger.info('Date string: {}'.format(date_string))
-    print 'Date string:', date_string
-else:
-    process_date = datetime.datetime.utcnow() - datetime.timedelta(days=1)
-    date_string = '{:02d}{:02d}{:02d}'.format(process_date.year,
-                                              process_date.month,
-                                              process_date.day)
-    logger.info('Date string: {}'.format(date_string))
-    print 'Date string:', date_string
+    if len(sys.argv) > 1:
+        date_string = sys.argv[1]
+        process_date = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+        logger.info('Date string: {}'.format(date_string))
+        print 'Date string:', date_string
+    else:
+        process_date = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+        date_string = '{:02d}{:02d}{:02d}'.format(process_date.year,
+                                                  process_date.month,
+                                                  process_date.day)
+        logger.info('Date string: {}'.format(date_string))
+        print 'Date string:', date_string
 
-results, scraperfilename = scraper_connection.main(process_date)
+    results, scraperfilename = scraper_connection.main(process_date)
 
-if scraperfilename:
-    logger.info("Scraper file name: " + scraperfilename)
-    print "Scraper file name:", scraperfilename
+    if scraperfilename:
+        logger.info("Scraper file name: " + scraperfilename)
+        print "Scraper file name:", scraperfilename
 
-logger.info("Running Mongo.formatter.py")
-print "Running Mongo.formatter.py"
-formatter.main(results, server_details, file_details, process_date,
-               date_string)
+    logger.info("Running Mongo.formatter.py")
+    print "Running Mongo.formatter.py"
+    formatted = formatter.main(results, server_details, file_details,
+                               process_date, date_string)
 
-logger.info("Running PETRARCH")
-print "Running PETRARCH"
-results = petrarch.run_pipeline(results, 'daily_events_YYMMDD.csv')
+    logger.info("Running PETRARCH")
+    print "Running PETRARCH"
+    file_details.fullfile_stem + date_string
+    if write_petr and not oneaday_filter:
+        petrarch.run_pipeline(formatted,
+                              '{}{}.txt'.format(file_details.fullfile_stem,
+                                                date_string))
+    elif write_petr and oneaday_filter:
+        print "Can't write to output and run the one-a-day filter. Exiting."
+        logger.warning("Can't write to output and run the one-a-day filter. Exiting.")
+        sys.exit()
+    elif not write_petr and oneaday_filter:
+        results = petrarch.run_pipeline(formatted, write_output=False)
+    else:
+        print "Can't run with the options you've specified. You need to fix something."
+        logger.warning("Can't run with the options you've specified. Exiting.")
+        sys.exit()
 
-logger.info("Running oneaday_formatter.py")
-print "Running oneaday_formatter.py"
-oneaday_formatter.main(date_string, server_details, file_details)
+    logger.info("Running oneaday_formatter.py")
+    print "Running oneaday_formatter.py"
+    oneaday_formatter.main(date_string, server_details, file_details)
 
-logger.info("Running phox_uploader.py")
-print "Running phox_uploader.py"
-uploader.main(date_string, server_details, file_details)
+    logger.info("Running phox_uploader.py")
+    print "Running phox_uploader.py"
+    uploader.main(date_string, server_details, file_details)
 
-logger.info('PHOX.pipeline end')
-print 'PHOX.pipeline end:', datetime.datetime.utcnow()
+    logger.info('PHOX.pipeline end')
+    print 'PHOX.pipeline end:', datetime.datetime.utcnow()
