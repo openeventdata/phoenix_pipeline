68692a336631f1441f104ca89e0decd1afb61a74
Parham Negahdar <pnegahdar@gmail.com>
2015-04-30 14:09:25 -0400

    Fix schedule

--- pipeline_sched.py
+++ pipeline_sched.py
@@ -5,5 +5,5 @@ from apscheduler.triggers.cron import CronTrigger
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
-    scheduler.add_job(run, CronTrigger(minute=0))
+    scheduler.add_job(run, CronTrigger(minute=0, hour=1))
     scheduler.start()
