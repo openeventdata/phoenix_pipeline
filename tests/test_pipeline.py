51fb87588673d30c457caaa32a5e48e8fba894bf
Andy Halterman <ahalterman0@gmail.com>
2016-06-29 12:34:31 -0400

    Add basic tests (#97)
    * Basic test of pipeline, Petr1 and Petr2
    
    * Make work with pytest
    
    * Add Travis, clean up formatting.

---
+++ tests/test_pipeline.py
@@ -0,0 +1,91 @@
+from bson.objectid import ObjectId
+import datetime
+from petrarch import petrarch
+from petrarch2 import petrarch2
+
+formatted = [{u'language': u'english', 
+u'title': u'6 killed in attacks in Iraqi capital Friday', 
+u'url': u'http://www.menafn.com/1094827896/6-killed-in-attacks-in-Iraqi-capital-Friday?src=RSS', 
+u'stanford': 1, 
+u'content': u'BAGHDAD: At least six people, including a soldier, were killed in a spate of attacks across Iraqi capital Baghdad on Friday.  A sniper opened fire on soldiers manning a checkpoint in southern Baghdad, killing a soldier and injuring three others, police officer Nader al-Janabi told Anadolu Agency.  Two civilians were killed and six others injured in a bomb blast in al-Zafarana district in south-eastern Baghdad, he said.  Three more civilians were killed and seven others injured in two bomb blasts in southern and northern Baghdad, according to al-Janabi.  Iraqi officials often blame the attacks on the Daesh terrorist group, which overran vast swathes of territory in Iraq in 2014. ', 
+u'source': u'menafn_iraq', 
+u'parsed_sents': [u'(ROOT (S (NP (NNP BAGHDAD)) (: :) (NP (NP (QP (IN At) (JJS least) (CD six)) (NNS people)) (, ,) (PP (VBG including) (NP (DT a) (NN soldier))) (, ,)) (VP (VBD were) (VP (VBN killed) (PP (IN in) (NP (NP (DT a) (NN spate)) (PP (IN of) (NP (NNS attacks))))) (PP (IN across) (NP (JJ Iraqi) (NN capital) (NNP Baghdad))) (PP (IN on) (NP (NNP Friday))))) (. .)))', 
+u'(ROOT (S (NP (DT A) (NN sniper)) (VP (VBD opened) (NP (NN fire)) (PP (IN on) (S (S (NP (NNS soldiers)) (VP (VBG manning) (NP (NP (DT a) (NN checkpoint)) (PP (IN in) (NP (JJ southern) (NNP Baghdad)))) (, ,) (S (VP (VP (VBG killing) (NP (DT a) (NN soldier))) (CC and) (VP (VBG injuring) (NP (CD three) (NNS others))))))) (, ,) (NP (NNS police) (NN officer) (NNP Nader) (NNP al-Janabi)) (VP (VBD told) (NP (NNP Anadolu) (NNP Agency))) (. .))))))', 
+u'(ROOT (S (S (NP (CD Two) (NNS civilians)) (VP (VBD were) (VP (VP (VBN killed)) (CC and) (NP (NP (CD six) (NNS others)) (VP (VBN injured) (PP (IN in) (NP (NP (DT a) (NN bomb) (NN blast)) (PP (IN in) (NP (NP (NN al-Zafarana) (NN district)) (PP (IN in) (NP (JJ south-eastern) (NNP Baghdad)))))))))))) (, ,) (NP (PRP he)) (VP (VBD said)) (.  .)))', 
+u'(ROOT (S (NP (CD Three) (JJR more) (NNS civilians)) (VP (VBD were) (VP (VP (VBN killed)) (CC and) (NP (NP (CD seven) (NNS others)) (VP (VBN injured) (PP (IN in) (NP (NP (CD two) (NN bomb) (NNS blasts)) (PP (IN in) (NP (ADJP (JJ southern) (CC and) (JJ northern)) (NNP Baghdad))))))) (, ,) (PP (VBG according) (PP (TO to) (NP (NNP al-Janabi)))))) (. .)))', 
+u'(ROOT (S (NP (JJ Iraqi) (NNS officials)) (ADVP (RB often)) (VP (VBP blame) (NP (NP (DT the) (NNS attacks)) (PP (IN on) (NP (NP (DT the) (NNP Daesh) (JJ terrorist) (NN group)) (, ,) (SBAR (WHNP (WDT which)) (S (VP (VBD overran) (NP (NP (JJ vast) (NNS swathes)) (PP (IN of) (NP (NP (NN territory)) (PP (IN in) (NP (NNP Iraq)))))) (PP (IN in) (NP (CD 2014)))))))))) (. .)))'], 
+u'date': u'160626', 
+u'date_added': datetime.datetime(2016, 6, 26, 19, 0, 17, 640000), 
+u'_id': ObjectId('57702641172ab87eb7dc98fa')}, 
+{u'language': u'english', 
+u'title': u'Soldiers, Policemen Fight Over Rice', 
+u'url': u'http://www.thetidenewsonline.com/2016/06/24/soldiers-policemen-fight-over-rice/', 
+u'stanford': 1, 
+u'content': u'There was chaos at the Borno State Government House in Maiduguri, yesterday, as soldiers and policemen engaged in gun battle over rice meant for internally displaced persons.  The Government House is besieged daily by thousands of internally displaced persons within Maiduguri metropolis, who choose to stay outside of the designated camps.  The IDPs, who queue for hours to receive rice and other relief items, often cause gridlock around the Government House with many of them having to go back empty handed each day.  The situation, however, turned violent, yesterday afternoon when the soldiers that were deployed to maintain law and order tried to benefit from the largese.  The soldiers were said to have tried to force their way into the Deputy Governor\u2019s office, the place designated for the distribution, to get their vehicles filled.  An attempt by the mobile policemen attached to the office to prevent the soldiers from achieving their goal led to a shootout.  It was gathered that the soldiers fired several warning shots and the mobile policemen shot back in return, while also firing canisters of tear gas.  Lucky Irabor, to get the furious soldiers to withdraw from the battle, which caused panic across Maiduguri.  It was gathered that Irabor, the most senior military officer around, and the Commissioner of Police, Aminchi Baraya, subsequently visited the injured policeman at the hospital.', 
+u'source': u'nigeria_tidenews', 
+u'parsed_sents': [u'(ROOT (S (NP (EX There)) (VP (VBD was) (NP (NP (NN chaos)) (PP (IN at) (NP (NP (DT the) (NNP Borno) (NNP State) (NNP Government) (NNP House)) (PP (IN in) (NP (NNP Maiduguri))))) (, ,) (NP (NN yesterday)) (, ,)) (PP (IN as) (NP (NP (NNS soldiers) (CC and) (NNS policemen)) (VP (VBN engaged) (PP (IN in) (NP (NP (NN gun) (NN battle)) (PP (IN over) (NP (NP (NN rice)) (VP (VBN meant) (PP (IN for) (NP (ADJP (RB internally) (JJ displaced)) (NNS persons)))))))))))) (. .)))', 
+u'(ROOT (S (NP (DT The) (NNP Government) (NNP House)) (VP (VBZ is) (VP (VBN besieged) (ADVP (RB daily)) (PP (IN by) (NP (NP (NNS thousands)) (PP (IN of) (NP (NP (ADJP (RB internally) (JJ displaced)) (NNS persons)) (PP (IN within) (NP (NP (NNP Maiduguri) (NN metropolis)) (, ,) (SBAR (WHNP (WP who)) (S (VP (VBP choose) (S (VP (TO to) (VP (VB stay) (ADVP (IN outside) (PP (IN of) (NP (DT the) (VBN designated) (NNS camps)))))))))))))))))) (. .)))', 
+u'(ROOT (NP (NP (NP (DT The) (NNS IDPs)) (, ,) (SBAR (WHNP (WP who)) (S (VP (VB queue) (SBAR (IN for) (S (NP (NNS hours)) (VP (TO to) (VP (VB receive) (NP (NP (NN rice) (CC and) (JJ other) (NN relief) (NNS items)) (, ,) (S (ADVP (RB often)) (VP (VBP cause) (NP (NN gridlock)) (PP (IN around) (S (NP (NP (DT the) (NNP Government) (NNP House)) (PP (IN with) (NP (NP (JJ many)) (PP (IN of) (NP (PRP them)))))) (VP (VBG having) (S (VP (TO to) (VP (VB go) (NP (ADJP (RB back) (JJ empty)) (NN handed)) (NP (DT each) (NN day))))))))))))))))))) (. .)))', 
+u'(ROOT (S (S (NP (DT The) (NN situation)) (, ,) (ADVP (RB however)) (, ,) (VP (VBD turned) (ADJP (JJ violent)))) (, ,) (NP (NP (NN yesterday) (NN afternoon)) (SBAR (WHADVP (WRB when)) (S (NP (NP (DT the) (NNS soldiers)) (SBAR (WHNP (WDT that)) (S (VP (VBD were) (VP (VBN deployed) (S (VP (TO to) (VP (VP (VB maintain) (NP (NN law))) (CC and) (VP (NN order) (VP (VBD tried) (S (VP (TO to) (VP (VB benefit) (PP (IN from) (NP (DT the) (NN largese))))))))))))))))))) (. .)))',
+u"(ROOT (S (NP (DT The) (NNS soldiers)) (VP (VBD were) (VP (VBN said) (S (VP (TO to) (VP (VB have) (VP (VBN tried) (S (VP (TO to) (VP (VB force) (NP (PRP$ their) (NN way)) (PP (IN into) (NP (NP (NP (DT the) (NNP Deputy) (NNP Governor) (POS 's)) (NN office)) (, ,) (NP (NP (DT the) (NN place)) (VP (VBN designated) (PP (IN for) (NP (DT the) (NN distribution))))) (, ,)))))) (S (VP (TO to) (VP (VB get) (S (NP (PRP$ their) (NNS vehicles)) (VP (VBN filled)))))))))))) (. .)))",
+u'(ROOT (S (NP (NP (DT An) (NN attempt)) (PP (IN by) (NP (NP (DT the) (JJ mobile) (NNS policemen)) (VP (VBN attached) (PP (TO to) (NP (DT the) (NN office))) (S (VP (TO to) (VP (VB prevent) (NP (DT the) (NNS soldiers)) (PP (IN from) (S (VP (VBG achieving) (NP (PRP$ their) (NN goal)))))))))))) (VP (VBD led) (PP (TO to) (NP (DT a) (NN shootout)))) (. .)))', 
+u'(ROOT (S (NP (PRP It)) (VP (VBD was) (VP (VBN gathered) (SBAR (IN that) (S (NP (DT the) (NNS soldiers)) (VP (VBD fired) (SBAR (S (NP (NP (JJ several) (VBG warning) (NNS shots)) (CC and) (NP (DT the) (JJ mobile) (NNS policemen))) (VP (VBD shot) (ADVP (RB back)) (PP (IN in) (NP (NN return))) (, ,) (SBAR (IN while) (S (ADVP (RB also)) (VP (NN firing) (NP (NP (NNS canisters)) (PP (IN of) (S (VP (VB tear) (NP (NN gas))))))))))))))))) (.  .)))'], 
+u'date': u'160624', 
+u'date_added': datetime.datetime(2016, 6, 26, 19, 0, 18), 
+u'_id': ObjectId('57702642172ab87eb5dc98e9')},
+{ "_id" : ObjectId("57702678172ab87ec2dc9933"), 
+"content" : "BAGHDAD - A senior Iraqi commander said the city of Fallujah was \"fully liberated\" from Islamic State of Iraq and Syria (ISIS) militants on Sunday, after a more than monthlong military operation.  Iraqi troops have entered the northwestern al-Julan neighborhood, the last area of Fallujah to remain under ISIS control, the head of the counterterrorism forces in the operation, Lt. Gen. Abdul-Wahab al-Saadi, told The Associated Press.  Al-Saadi said the operation, which began in late May, \"is done and the city is fully liberated.\" The Iraqi army was backed by U.S.-led airstrikes and paramilitary troops, mostly Shiite militias.  \"From the center of al-Julan neighborhood, we congratulate the Iraqi people and the commander in chief...and declare that the Fallujah fight is over,\" he told Iraqi state TV, flanked by military officers and soldiers. Some of the soldiers were shooting in the air, chanting and waving the Iraqi flag.  He added that troops will start working on removing bombs from the city's streets and buildings.  In a statement, the U.S. central military command overseeing the U.S.-led coalition in Iraq said: \"The Coalition continues to provide support through strikes, intelligence, and advice and assistance to the Iraqi Security Forces operating in Fallujah and will continue to do so through deliberate clearing operations.\"  Prime Minister Haider al-Abadi declared victory in Fallujah over a week ago, after Iraqi forces advanced into the city center and took control of a government complex. He pledged that remaining pockets of ISIS fighters would be cleared out within hours, but fierce clashes on the city's northern and western edges persisted for days.  Tens of thousands of people have fled the fighting, overwhelming camps for the displaced run by the government and aid groups.  According to the U.N. refugee agency, more than 85,000 people have fled Fallujah and the surrounding area since the offensive began. The UNHCR and others have warned of dire conditions in the camps -- where temperatures are well over 40 degrees Celsius (104 F) and shelter is limited -- and have called for more funds to meet mounting needs.  Fallujah, which is located about 40 miles west of Baghdad, was the first city to fall to IS, in January 2014.  Fallujah was also a stronghold of Sunni insurgents following the U.S.-led invasion in 2003. More than 100 American soldiers died and hundreds more were wounded in intense, house-by-house fighting in Fallujah in 2004.  ISIS extremists still control significant areas in northern and western Iraq, including the country's second-largest city, Mosul. The group declared an Islamic caliphate on the territory it holds in Iraq and Syria and at the height of its power was estimated to hold nearly a third of each country.  More than 3.3 million Iraqis have fled their homes since ISIS swept across northern and western Iraq in the summer of 2014, according to U.N. figures. More than 40 percent of the displaced are from Anbar province, where Fallujah is located.", 
+"source" : "cbs_world", 
+"date" : "Sun, 26 Jun 2016 17:37:27 -0400", 
+"language" : "english", 
+"title" : "Iraq: Fallujah \"fully liberated\" after monthlong fight", 
+"url" : "http://www.cbsnews.com/news/iraqi-commander-fallujah-fully-liberated-after-a-month/", 
+"date_added" : datetime.datetime(2016, 6, 26, 19, 0, 18), 
+"stanford" : 1, 
+"parsed_sents" : [ "(ROOT (S (NP (NNP BAGHDAD) (: -) (NN A) (JJ senior) (JJ Iraqi) (NN commander)) (VP (VBD said) (SBAR (S (NP (NP (DT the) (NN city)) (PP (IN of) (NP (NNP Fallujah)))) (VP (VBD was) (`` ``) (VP (ADVP (RB fully)) (VBN liberated) ('' '') (PP (IN from) (NP (NP (JJ Islamic) (NN State) (PP (IN of) (NP (NP (NNP Iraq)) (CC and) (NP (NNP Syria) (PRN (-LRB- -LRB-) (NNP ISIS) (-RRB- -RRB-)) (NNS militants))))) (PP (IN on) (NP (NNP Sunday)))))) (, ,) (PP (IN after) (NP (DT a) (ADVP (JJR more) (IN than)) (JJ monthlong) (JJ military) (NN operation))))))) (. .)))",
+"(ROOT (S (S (NP (JJ Iraqi) (NNS troops)) (VP (VBP have) (VP (VBN entered) (NP (DT the) (JJ northwestern) (JJ al-Julan) (NN neighborhood))))) (, ,) (NP (NP (NP (DT the) (JJ last) (NN area)) (PP (IN of) (NP (NNP Fallujah))) (S (VP (TO to) (VP (VB remain) (PP (IN under) (NP (NNP ISIS) (NN control))))))) (, ,) (NP (NP (DT the) (NN head)) (PP (IN of) (NP (NP (DT the) (NN counterterrorism) (NNS forces)) (PP (IN in) (NP (DT the) (NN operation)))))) (, ,) (NP (NNP Lt.) (NNP Gen.) (NNP Abdul-Wahab) (NNP al-Saadi)) (, ,)) (VP (VBD told) (NP (DT The) (NNP Associated) (NNP Press))) (. .)))",
+"(ROOT (S (NP (NNP Al-Saadi)) (VP (VBD said) (SBAR (S (S (NP (NP (DT the) (NN operation)) (, ,) (SBAR (WHNP (WDT which)) (S (VP (VBD began) (PP (IN in) (NP (JJ late) (NNP May)))))) (, ,)) (`` ``) (VP (VBZ is) (VP (VBN done)))) (CC and) (S (NP (DT the) (NN city)) (VP (VBZ is) (ADVP (RB fully)) (VP (VBN liberated))))))) (. .) ('' '')))",
+"(ROOT (S (`` ``) (S (PP (IN From) (NP (NP (DT the) (NN center)) (PP (IN of) (NP (JJ al-Julan) (NN neighborhood))))) (, ,) (NP (PRP we)) (VP (VP (VBP congratulate) (NP (NP (DT the) (JJ Iraqi) (NNS people)) (CC and) (NP (NP (DT the) (NN commander)) (PP (IN in) (NP (NN chief)))))) (: ...) (CC and) (VP (VB declare) (SBAR (IN that) (S (NP (DT the) (NNP Fallujah) (NN fight)) (VP (VBZ is) (ADVP (IN over)))))))) (, ,) ('' '') (NP (PRP he)) (VP (VBD told) (NP (JJ Iraqi) (NN state) (NN TV)) (, ,) (S (VP (VBN flanked) (PP (IN by) (NP (JJ military) (NNS officers) (CC and) (NNS soldiers)))))) (. .)))",
+"(ROOT (S (PP (IN In) (NP (DT a) (NN statement))) (, ,) (NP (NP (DT the) (NNP U.S.) (JJ central) (JJ military) (NN command)) (VP (VBG overseeing) (NP (NP (DT the) (JJ U.S.-led) (NN coalition)) (PP (IN in) (NP (NNP Iraq)))))) (VP (VBD said) (: :) (`` ``) (S (NP (DT The) (NNP Coalition)) (VP (VP (VBZ continues) (S (VP (TO to) (VP (VB provide) (NP (NN support)) (PP (IN through) (NP (NP (NP (NNS strikes)) (, ,) (NP (NN intelligence)) (, ,) (CC and) (NP (NN advice))) (CC and) (NP (NP (NN assistance)) (PP (TO to) (NP (NP (DT the) (JJ Iraqi) (NN Security) (NNS Forces)) (VP (VBG operating) (PP (IN in) (NP (NNP Fallujah))))))))))))) (CC and) (VP (MD will) (VP (VB continue) (S (VP (TO to) (VP (VB do) (ADVP (RB so))))) (PP (IN through) (NP (JJ deliberate) (NN clearing) (NNS operations)))))))) (. .) ('' '')))",
+"(ROOT (S (NP (PRP He)) (VP (VP (VBD pledged) (SBAR (IN that) (S (NP (NP (VBG remaining) (NNS pockets)) (PP (IN of) (NP (NNP ISIS) (NNS fighters)))) (VP (MD would) (VP (VB be) (VP (VBN cleared) (PRT (RP out)) (PP (IN within) (NP (NNS hours))))))))) (, ,) (CC but) (S (NP (NP (JJ fierce) (NNS clashes)) (PP (IN on) (NP (NP (DT the) (NN city) (POS 's)) (ADJP (JJ northern) (CC and) (JJ western)) (NNS edges)))) (VP (VBD persisted) (PP (IN for) (NP (NNS days)))))) (. .)))",
+"(ROOT (S (NP (NP (NNS Tens)) (PP (IN of) (NP (NP (NNS thousands)) (PP (IN of) (NP (NNS people)))))) (VP (VBP have) (VP (VBN fled) (NP (NP (DT the) (NN fighting)) (, ,) (NP (NP (JJ overwhelming) (NNS camps)) (PP (IN for) (NP (DT the) (JJ displaced) (NN run)))) (PP (IN by) (NP (DT the) (NN government) (CC and) (NN aid) (NNS groups)))))) (. .)))" ] }]
+
+def test_petr1_formatted_to_results():
+    petr1_results = petrarch.run_pipeline(formatted, write_output=False,
+                                                 parsed=True)
+    correct1_results = {'57702678172ab87ec2dc9933': 
+            [(u'20160626', u'IRQ', u'MED', u'010', u'57702678172ab87ec2dc9933_1',
+                'http://www.cbsnews.com/news/iraqi-commander-fallujah-fully-liberated-after-a-month/', 
+                'cbs_world'), 
+             (u'20160626', u'IRQMIL', u'IRQ', u'010', u'NAMED_TERROR_GROUP,1', 
+                u'57702678172ab87ec2dc9933_0', 
+                'http://www.cbsnews.com/news/iraqi-commander-fallujah-fully-liberated-after-a-month/', 
+                'cbs_world')
+              ]}
+    assert petr1_results == correct1_results
+
+def test_petr2_formatted_to_results():
+    petr2_results = petrarch2.run_pipeline(formatted, write_output=False,
+                                                 parsed=True)
+    correct2_results = {'57702678172ab87ec2dc9933': 
+          [(u'20160626', u'IRQMIL', u'MED', u'010', u'57702678172ab87ec2dc9933_1', 
+              'http://www.cbsnews.com/news/iraqi-commander-fallujah-fully-liberated-after-a-month/', 
+              'cbs_world'), 
+           (u'20160626', u'IRQMIL', u'IRQ', u'010', u'NAMED_TERROR_GROUP,1', u'57702678172ab87ec2dc9933_0', 
+              'http://www.cbsnews.com/news/iraqi-commander-fallujah-fully-liberated-after-a-month/', 
+              'cbs_world')
+           ], 
+      '57702642172ab87eb5dc98e9': 
+         [(u'20160624', u'NGAPPL', u'---GOV', u'191', u'REFUGEES,1', u'57702642172ab87eb5dc98e9_1', 
+             u'http://www.thetidenewsonline.com/2016/06/24/soldiers-policemen-fight-over-rice/', 
+             u'nigeria_tidenews')], 
+      '57702641172ab87eb7dc98fa': 
+         [(u'20160626', u'IRQ', u'IMGMUSISIUAF', u'111', u'TERROR,1', 
+            u'57702641172ab87eb7dc98fa_4', 
+            u'http://www.menafn.com/1094827896/6-killed-in-attacks-in-Iraqi-capital-Friday?src=RSS', 
+            u'menafn_iraq'), 
+         (u'20160626', u'---CVL', u'IRQ', u'190', u'57702641172ab87eb7dc98fa_3', 
+           u'http://www.menafn.com/1094827896/6-killed-in-attacks-in-Iraqi-capital-Friday?src=RSS', 
+           u'menafn_iraq')]}
+    assert petr2_results == correct2_results
+
